a143a42b510af0c3c6c6e0b40eed13f8ad087350
GUAN MING <guanmingchiu@gmail.com>
2024-04-21 20:47:41 +0800

    Create 1.two-sum.ts

---
+++ 1.two-sum.ts
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode id=1 lang=typescript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const map = new Map<number, number>();
+  for (let index = 0; index < nums.length; index++) {
+    if (map.get(nums[index]) !== undefined) {
+      return [map.get(nums[index])!, index];
+    }
+
+    map.set(target - nums[index], index);
+  }
+
+  return [];
+}
+// @lc code=end
+
+/* Better Solution
+function twoSum(nums: number[], target: number): number[] {
+    const numMap = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const num = nums[i];
+        const difference = target - num;
+
+        if (numMap.has(difference)) return [numMap.get(difference), i];
+        else numMap.set(num, i);
+    }
+    
+    return []
+};
+*/
