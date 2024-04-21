48c9f2fa58ead6fa57ff1731905b322e457f10aa
GUAN MING <guanmingchiu@gmail.com>
2024-04-21 20:48:09 +0800

    Create 9.palindrome-number.ts

---
+++ 9.palindrome-number.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=9 lang=typescript
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  return String(x).split("").reverse().join("") === String(x);
+}
+// @lc code=end
+
+/* Better Solution
+function isPalindrome(x: number): boolean {
+    let number = x.toString()
+
+    for (let i = 0; i < number.length; ++i) {
+        if (number[i] === number[number.length - i - 1]) {
+            continue
+        }
+
+        return false 
+    }
+
+    return true 
+};
+*/
