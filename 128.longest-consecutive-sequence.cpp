7a80c07012be3b13127622ac18ffbdd6160aedf0
GUAN MING <guanmingchiu@gmail.com>
2024-12-23 12:15:43 +0800

    🟡 Add 128.longest-consecutive-sequence.cpp

--- 128.longest-consecutive-sequence.cpp
+++ 128.longest-consecutive-sequence.cpp
@@ -34,7 +34,7 @@ public:
         nums.erase(unique(nums.begin(), nums.end()), nums.end());
 
         int cnt = 1, ans = 1;
-
+        
         for (int i = 0; i < nums.size() - 1; i++) {
             if (nums[i] + 1 == nums[i+1]) {
                 cnt += 1;
